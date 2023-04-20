055ce8ae3b5a2b9441aebdc8a268ee1fda5cac34
Dtikoli <tikolidavid@yahoo.com>
2023-04-20 06:32:37 -0700

    Final edit to file input_output func

--- custom_simple_shell/io_func.c
+++ custom_simple_shell/io_func.c
@@ -7,19 +7,19 @@
  */
 char *hist_get(info_t *info)
 {
-	char *buf, *dir;
+	char *buff, *dir;
 
 	dir = get_env(info, "HOME=");
 	if (!dir)
 		return (NULL);
-	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-	if (!buf)
+	buff = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buff)
 		return (NULL);
-	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	_strcat(buf, HIST_FILE);
-	return (buf);
+	buff[0] = 0;
+	_strcpy(buff, dir);
+	_strcat(buff, "/");
+	_strcat(buff, HIST_FILE);
+	return (buff);
 }
 
 /**
@@ -30,19 +30,19 @@ char *hist_get(info_t *info)
 int hist_write(info_t *info)
 {
 	ssize_t fd;
-	char *filename = hist_get(info);
-	list_t *node = NULL;
+	char *fname = hist_get(info);
+	list_t *current = NULL;
 
-	if (!filename)
+	if (!fname)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-	free(filename);
+	fd = open(fname, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(fname);
 	if (fd == -1)
 		return (-1);
-	for (node = info->history; node; node = node->next)
+	for (current = info->history; current; current = current->next)
 	{
-		fd_puts(node->str, fd);
+		fd_puts(current->str, fd);
 		fd_putc('\n', fd);
 	}
 	fd_putc(BUFF_FLUSH, fd);
@@ -58,39 +58,39 @@ int hist_write(info_t *info)
 int hist_read(info_t *info)
 {
 	int i, last = 0, linecount = 0;
-	ssize_t fd, rdlen, fsize = 0;
+	ssize_t fd, len, fsize = 0;
 	struct stat st;
-	char *buf = NULL, *filename = hist_get(info);
+	char *buff = NULL, *fname = hist_get(info);
 
-	if (!filename)
+	if (!fname)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	free(filename);
+	fd = open(fname, O_RDONLY);
+	free(fname);
 	if (fd == -1)
 		return (0);
 	if (!fstat(fd, &st))
 		fsize = st.st_size;
 	if (fsize < 2)
 		return (0);
-	buf = malloc(sizeof(char) * (fsize + 1));
-	if (!buf)
+	buff = malloc(sizeof(char) * (fsize + 1));
+	if (!buff)
 		return (0);
-	rdlen = read(fd, buf, fsize);
-	buf[fsize] = 0;
-	if (rdlen <= 0)
-		return (free(buf), 0);
+	len = read(fd, buff, fsize);
+	buff[fsize] = 0;
+	if (len <= 0)
+		return (free(buff), 0);
 	close(fd);
 	for (i = 0; i < fsize; i++)
-		if (buf[i] == '\n')
+		if (buff[i] == '\n')
 		{
-			buf[i] = 0;
-			hist_build(info, buf + last, linecount++);
+			buff[i] = 0;
+			hist_build(info, buff + last, linecount++);
 			last = i + 1;
 		}
 	if (last != i)
-		hist_build(info, buf + last, linecount++);
-	free(buf);
+		hist_build(info, buff + last, linecount++);
+	free(buff);
 	info->histcount = linecount;
 	while (info->histcount-- >= HIST_MAX)
 		delete_node_index(&(info->history), 0);
@@ -101,20 +101,20 @@ int hist_read(info_t *info)
 /**
  * hist_build - for adding new entries to history list
  * @info: Struct containing potential arguments.
- * @buf: history buffer
+ * @buff: history buffer
  * @linecount: history line count
  * Return: Always 0
  */
-int hist_build(info_t *info, char *buf, int linecount)
+int hist_build(info_t *info, char *buff, int linecount)
 {
-	list_t *node = NULL;
+	list_t *current = NULL;
 
 	if (info->history)
-		node = info->history;
-	add_node_end(&node, buf, linecount);
+		current = info->history;
+	add_node_end(&current, buff, linecount);
 
 	if (!info->history)
-		info->history = node;
+		info->history = current;
 	return (0);
 }
 
@@ -125,13 +125,14 @@ int hist_build(info_t *info, char *buf, int linecount)
  */
 int hist_renum(info_t *info)
 {
-	list_t *node = info->history;
-	int i = 0;
+	list_t *current = info->history;
+	int linecount = 0;
 
-	while (node)
+	while (current)
 	{
-		node->num = i++;
-		node = node->next;
+		current->num = linecount++;
+		current = current->next;
 	}
-	return (info->histcount = i);
+	return (info->histcount = linecount);
 }
+
